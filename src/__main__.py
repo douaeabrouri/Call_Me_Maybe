f3bc4299275ae3fd7e567cd5777dc169ec417514
douae abrouri <abrouridoae@gmail.com>
2026-05-09 18:49:42 +0000

    add src folder

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,34 @@ from llm_sdk.llm_sdk import Small_LLM_Model
 
 def main() -> None:
     model = Small_LLM_Model()
-    print("modeal loaded successufully")
+    # print("modeal loaded successufully")
+    # text: str = "c est quoi la somme de 3 et 5?"
+    # token_ids  = model.encode(text)
+    # print(f"original text -----> {text}")
+    # print(f"token ids -----> {token_ids}")
+    # decode_txt = model.decode(token_ids)
+    # print(f"decode ------->{decode_txt}")
+
+    # with open("function_calling_tests.json", 'r') as file:
+    tests = [
+        "{",
+        "}",
+        '"',
+        '"name"',
+        ":",
+        ",",
+        "fn_add_numbers",
+        "hello",
+        "42",
+    ]
+    for text in tests:
+        token_ids = model.encode(text)
+        print(f"Text ------------>{text}")
+        print(f"Token ids ----------->{token_ids}")
+        decoded = model.decode(token_ids)
+        print(f"Decode ------------->{decoded}")
+        print("-" * 40)
+
 
 if __name__ == "__main__":
    main()
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from llm_sdk.llm_sdk import Small_LLM_Model
-
-def main() -> None:
-    model = Small_LLM_Model()
-    print("modeal loaded successufully")
-
-if __name__ == "__main__":
-   main()
